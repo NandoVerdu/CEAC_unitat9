5939eff02342dbdabb3bb5307f41601033e1199e
NandoVerdu <96000349+NandoVerdu@users.noreply.github.com>
2024-05-01 15:51:00 +0000

    progress component study

---
+++ src/lib/components/progress.svelte
@@ -0,0 +1,15 @@
+<!-- src/progress.svelte -->
+<script>
+   import { Slide } from '@components'
+   import { animate, signal } from '@motion'
+ 
+   export let progress = signal(0)
+   
+   animate(async() => {
+     await progress.to(10_000)
+   })
+ </script>
+ 
+ <Slide>
+   {$progress.toLocaleString('en', { maximumFractionDigits: 0 })}
+ </Slide>
