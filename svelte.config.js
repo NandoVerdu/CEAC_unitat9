b24feb335ada4ec06573b0f1f42bb9d37ad6fdb0
NandoVerdu <96000349+NandoVerdu@users.noreply.github.com>
2024-05-03 11:51:47 +0000

    nojekyll step

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-export default {
-	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
-}
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({pages: "docs"}),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/CEAC_UNITAT9' : '',
+        }
+    }
+};
+
+export default config;
